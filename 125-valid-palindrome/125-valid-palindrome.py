4a3fac0cae4b1901d9ef6e7f0f4a5525117b9f9e
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-07-13 01:20:50 +0900

    Time: 60 ms (73.81%), Space: 14.3 MB (85.65%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -19,7 +19,8 @@ class Solution:
                 new_s += c.lower()
         
         # 팰린드럼 판별
-        if new_s == ''.join(reversed(new_s)):
+        # if new_s == ''.join(reversed(new_s)): # 55~102ms
+        if new_s == new_s[::-1]:
             return 1
         else:
             return 0
