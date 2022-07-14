12b244801716efcfbdc8f8fd2813a96e41433b4c
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-07-14 22:26:34 +0900

    Create 344_hoyeong.py

---
+++ String/[Leet] 344_Reverse String/344_hoyeong.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        내장함수 사용 뒤집기
+        """
+        s.reverse()
