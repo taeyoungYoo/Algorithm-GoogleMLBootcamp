7f714e6a8e16721e8149243da5bdda5c540ce9e7
TaeyoungYoo <yty7320@gmail.com>
2022-07-28 20:35:02 +0900

    week2 taeyoung time complexity modified

--- Random/Programmers/42587_프린터/42587_taeyoung.py
+++ Random/Programmers/42587_프린터/42587_taeyoung.py
@@ -3,8 +3,8 @@ def solution(priorities, location):
     priority_index = [x for x in range(len(priorities))]
 
     while len(priorities) > 1:
-        ready_val = priorities.pop(0) # O(1)
-        ready_idx = priority_index.pop(0) # O(1)
+        ready_val = priorities.pop(0) # O(N)
+        ready_idx = priority_index.pop(0) # O(N)
         max_val = max(priorities) # O(N)
         if max_val > ready_val:
             priorities.append(ready_val)
