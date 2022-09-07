b7256e998a8e6e78b27e46c48a525034b67b5000
YSS_Home <dbstjdtlr11@naver.com>
2022-09-08 01:22:35 +0900

    solve

--- Random/Programmers/Kakao_2022_Blind/k진수에서소수개수구하기/sungsik_k진수.py
+++ Random/Programmers/Kakao_2022_Blind/k진수에서소수개수구하기/sungsik_k진수.py
@@ -1,15 +1,31 @@
-def convert_n_to_k(n, k):  # n을 k진수로 반환
-  ret = ""
-  while n > 0:
-    ret += str(n % k)
-    n = n // k
-  return ''.join(reversed(ret))
-
 def solution(n, k):
-  answer = -1
+  answer = 0
+  converted = ""
+  while n:  # 숫자를 k진법으로 변환
+    converted = str(n % k) + converted
+    n = n // k
 
+  converted = converted.split("0")
+
+  for num in converted:
+    if num == '':
+      continue
+    if int(num) < 2:
+      continue
+
+    prime = True
+    for i in range(2, int(int(num) ** 0.5) + 1):
+      if int(num) % i == 0:
+        prime = False
+        break
+
+    if prime:
+      answer += 1
 
   return answer
 
+n = 437674
+k = 3
+# expect : 3
 
-
+print(solution(n, k))
