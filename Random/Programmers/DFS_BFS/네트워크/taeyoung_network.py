908ecac8257ff5241b6d143401fe6f6ddaf3e401
TaeyoungYoo <yty7320@gmail.com>
2022-09-29 17:18:11 +0900

    programmers network dfs approach

---
+++ Random/Programmers/DFS_BFS/네트워크/taeyoung_network.py
@@ -0,0 +1,32 @@
+'''
+    프로그래머스 네트워크
+    - 사이클의 개수를 세는 문제
+    - 1 사이클이 곧 1 네트워크
+    - DFS 사용
+'''
+
+visited = []
+def dfs(node, graph):
+    global visited
+    visited[node] = 1
+    for next in graph[node]:
+        if visited[next] == 0:
+            dfs(next, graph)
+
+def solution(n, computers):
+    global visited
+    visited = [0] * n
+    graph = [[] for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            if computers[i][j] == 1:
+                graph[i].append(j)
+    answer = 0
+    for i in range(n):
+        if visited[i] == 0:
+            dfs(i, graph)
+            answer += 1
+    return answer
